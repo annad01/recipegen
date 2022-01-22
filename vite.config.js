f7156e14dcf7eddf8b5a67982ac5e3ac0144fbac
Anna Duong <annaduong1001@gmail.com>
2022-01-21 22:10:41 -0500

    Modify vite config to avoid hardcode

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import { minifyHtml, injectHtml } from 'vite-plugin-html'
 const scalaVersion = '2.13'
 
 export default ({mode}) => {
-    //const mainJS = `/target/scala-${scalaVersion}/root-${mode === 'production' ? 'opt' : 'fastopt'}/main.js`
-    const mainJS = `/target/scala-${scalaVersion}/root-fastopt/main.js`
+    const mainJS = `/target/scala-${scalaVersion}/root-${mode === 'production' ? 'opt' : 'fastopt'}/main.js`
+    console.log('mainJS', mainJS)
     const script = `<script type="module" src="${mainJS}"></script>`
 
     return {
